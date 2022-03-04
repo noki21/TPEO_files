44689a7eee81a58f682742a12e3ee9084b4ffa7d
noki21 <noahkim02410@gmail.com>
2022-03-03 18:59:58 -0600

    Create class_2.js

---
+++ class_2.js
@@ -0,0 +1,13 @@
+function triangleArea(base, height) {
+    return base * height * .5;
+}
+
+var area = triangleArea(5,10);
+var areaAgain = triangleArea(2,4);
+var areaFinal = triangleArea(4,20);
+
+console.log(area);
+console.log(areaAgain);
+console.log(areaFinal);
+
+console.log('helllo')
